e0801cbe962810ee3315d26efbf11691732d9c3f
Daniel <Daniel@192.168.1.129>
2019-05-20 18:16:25 +0200

    implementar los metodos.
    Closes #5.

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -19,21 +19,52 @@ public class Coche {
 		modelo = mo;
 		fabricante = f;
 	}
+	
+
+
+	
+	public String GasolinaCoche /* este seria el metodo a */(String modelo, String fabricante)
+	{
+	
+		String resultado =" es un "+ fabricante + modelo + "y gasta 1,337 euros por litro.";
+		return resultado;
+	}
+	
+
+
+	
+	public String DieselCoche /* este seria el metodo b */(String modelo, String fabricante)
+	{
+		String resultado =" es un "+ fabricante + modelo + "y gasta 1,052 euros por litro.";
+		return resultado;
+	}
+	
+
+	
+	
+	public String HibridoCoche /* este seria el metodo c */ (String modelo, String fabricante)
+	{
+		String resultado =" es un "+ fabricante + modelo + "y no necesita combustible.";
+		return resultado;
+	}
+	
+	
+	
+	
 
 	public String metodo1() {
 		String resultado = "";
 		if (atrib == "Gasolina") {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoA(modelo, fabricante);
+			resultado += "El coche con matricula " + GasolinaCoche(modelo, fabricante);
+			
 		} else if (atrib == "Diesel") {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoB(modelo, fabricante);
+			resultado += "El coche con matricula " + DieselCoche(modelo, fabricante);
+		
 		} else if (atrib == "Híbrido") { 
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += metodoC(modelo, fabricante);
+			resultado += "El coche con matricula " + HibridoCoche(modelo, fabricante);
+		
 		} else {
-			resultado += "El coche con matricula " + matricula + " ";
-			resultado += "no dispone de información";
+			resultado += "El coche con matricula " + "no dispone de información";
 		}
 		return resultado;
 	}
