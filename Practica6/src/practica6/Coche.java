27219806ff313bca067823ecfe7015331600e80c
Daniel <Daniel@192.168.1.129>
2019-05-20 19:22:24 +0200

    Documentar siguiendo la herramienta Javadoc.
    Closes #6

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -1,7 +1,15 @@
 package practica6;
 
 public class Coche {
-	
+	/**
+	 * 
+	 * @author Daniel Rodríguez Cea
+	 * 
+	 * Clase Coche en la que definimos los atributos del coche y después le implementamos los metodos de TipoDeCombustible
+	 * 
+	 * GasolinaCoche, DieselCoche, HibridoCoche y MatriculaCoche.
+	 *
+	 */
 
 	//creamos la constante
 	private static final String EL_COCHE_CON_MATRICULA = "El coche con matricula";
@@ -26,7 +34,14 @@ public class Coche {
 		fabricante=f;
 	}
 	
-
+	//Método que nos devuelve la matrícula del coche.
+	
+	
+	/**
+	* 
+	* @return cadena con la matrícula del coche
+	* 
+	*/
 
 	private String MatriculaCoche() {
 		return EL_COCHE_CON_MATRICULA + matricula + " ";
@@ -34,33 +49,64 @@ public class Coche {
 	
 	
 	
+	//Método que devuelve el resultado de los coches que utilizan gasolina
+	/**
+	 * 
+	 * @param modelo le pasamos el tipo de modelo del coche.
+	 * 
+	 * @param fabricante le pasamos el tipo de fabricante del coche.
+	 * 
+	 * @return Devuelve el consumo de combustible del coche que utiliza gasolina.
+	*/
 
 	
-	public String GasolinaCoche /* este seria el metodo a */ (String modelo, String fabricante)
+	public String GasolinaCoche (String modelo, String fabricante)
 	{
 	
 		String resultado =" es un "+ fabricante + modelo + "y gasta 1,337 euros por litro.";
 		return resultado;
 	}
 	
-
+	//Método que devuelve el resultado de los coches que utilizan Diesel
+	/**
+	 * 
+	 * @param modelo le pasamos el tipo de modelo del coche.
+	 * 
+	 * @param fabricante le pasamos el tipo de fabricante del coche.
+	 * 
+	 * @return Devuelve el consumo de combustible del coche que utiliza Diesel.
+	 */
 
 	
-	public String DieselCoche /* este seria el metodo b */(String modelo, String fabricante)
+	public String DieselCoche (String modelo, String fabricante)
 	{
 		String resultado =" es un "+ fabricante + modelo + "y gasta 1,052 euros por litro.";
 		return resultado;
 	}
 	
-
+	//Método que devuelve el resultado de los coches hibridos
+	/**
+	 * 
+	 * @param modelo le pasamos el tipo de modelo del coche.
+	 * 
+	 * @param fabricante le pasamos el tipo de fabricante del coche.
+	 * 
+	 * @return Devuelve que no necesita cumbustible.
+	 */
 	
-	public String HibridoCoche /* este seria el metodo c */(String modelo, String fabricante)
+	
+	public String HibridoCoche (String modelo, String fabricante)
 	{
 		String resultado =" es un "+ fabricante + modelo + "y no necesita combustible.";
 		return resultado;
 	}
 	
-
+	//Método que según el tipo de combustible que utiliza coche nos devuelve una cadena.
+	/**
+	 * 
+	 * @return reultado es la información de cada coche según los atributos.
+	 *
+	 */
 	
 	public String tipoDeCombustible() {
 		String resultado = "";
